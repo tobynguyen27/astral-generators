19d8b4f175656859a595d00fe97ef362f3406381
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-01 16:55:19 +0700

    fluid

---
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/AGFluids.kt
@@ -0,0 +1,17 @@
+package dev.tobynguyen27.astralgenerators.contents
+
+import dev.tobynguyen27.astralgenerators.AstralGenerators.REGISTRATE
+import dev.tobynguyen27.astralgenerators.contents.tags.FluidTags
+import dev.tobynguyen27.astralgenerators.utils.Identifier
+import net.minecraft.tags.FluidTags as MCFluidTags
+
+object AGFluids {
+    val STEAM =
+        REGISTRATE.fluid("steam", Identifier("fluid/steam"), Identifier("fluid/steam_flow"))
+            .noBucket()
+            .tag(FluidTags.STEAM)
+            .removeTag(MCFluidTags.WATER)
+            .register()
+
+    fun register() {}
+}
