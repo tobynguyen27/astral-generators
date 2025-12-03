c3cced64baf178aa0c63cc633775732ec5563a89
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-04 00:06:12 +0700

    add bus blocks

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/client/AGMenuScreens.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/client/AGMenuScreens.kt
@@ -2,9 +2,8 @@ package dev.tobynguyen27.astralgenerators.client
 
 import dev.tobynguyen27.astralgenerators.contents.machines.assembler.AssemblerScreen
 import dev.tobynguyen27.astralgenerators.contents.machines.boiler_controller.BoilerControllerScreen
+import dev.tobynguyen27.astralgenerators.contents.ports.BusBlockScreen
 import dev.tobynguyen27.astralgenerators.gui.AGMenus
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.minecraft.client.gui.screens.MenuScreens
 
 object AGMenuScreens {
@@ -14,6 +13,9 @@ object AGMenuScreens {
         }
         MenuScreens.register(AGMenus.BOILER_CONTROLLER) { type, playerInventory, title ->
             BoilerControllerScreen(type, playerInventory, title)
+        }
+        MenuScreens.register(AGMenus.BASIC_INPUT_BUS) { type, playerInventory, title ->
+            BusBlockScreen(type, playerInventory, title)
         }
     }
 }
