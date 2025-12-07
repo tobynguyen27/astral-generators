569f30720d550c880dab178c8c7d331aae59a8e9
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-07 14:17:30 +0700

    clean code

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/hooks/EnergyAPI.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/hooks/EnergyAPI.kt
@@ -2,20 +2,9 @@ package dev.tobynguyen27.astralgenerators.hooks
 
 import dev.tobynguyen27.astralgenerators.contents.AGBlockEntities
 import dev.tobynguyen27.astralgenerators.contents.machines.assembler.AssemblerBlockEntity
-import dev.tobynguyen27.astralgenerators.contents.ports.hatches.energy.EnergyHatchBlockEntity
 import team.reborn.energy.api.EnergyStorage
 
 object EnergyAPI {
-
-    private val ENERGY_HATCHES =
-        listOf(
-            AGBlockEntities.BASIC_ENERGY_INPUT_HATCH,
-            AGBlockEntities.ADVANCED_ENERGY_INPUT_HATCH,
-            AGBlockEntities.INDUSTRIAL_ENERGY_INPUT_HATCH,
-            AGBlockEntities.BASIC_ENERGY_OUTPUT_HATCH,
-            AGBlockEntities.ADVANCED_ENERGY_OUTPUT_HATCH,
-            AGBlockEntities.INDUSTRIAL_ENERGY_OUTPUT_HATCH,
-        )
 
     fun init() {
         EnergyStorage.SIDED.registerForBlockEntity(
@@ -23,11 +12,23 @@ object EnergyAPI {
             AGBlockEntities.ASSEMBLER.get(),
         )
 
-        ENERGY_HATCHES.forEach {
-            EnergyStorage.SIDED.registerForBlockEntity(
-                { blockEntity: EnergyHatchBlockEntity, _ -> blockEntity.energyStorage },
-                it.get(),
+        initEnergyHatches()
+    }
+
+    private fun initEnergyHatches() {
+        listOf(
+                AGBlockEntities.BASIC_ENERGY_INPUT_HATCH,
+                AGBlockEntities.ADVANCED_ENERGY_INPUT_HATCH,
+                AGBlockEntities.INDUSTRIAL_ENERGY_INPUT_HATCH,
+                AGBlockEntities.BASIC_ENERGY_OUTPUT_HATCH,
+                AGBlockEntities.ADVANCED_ENERGY_OUTPUT_HATCH,
+                AGBlockEntities.INDUSTRIAL_ENERGY_OUTPUT_HATCH,
             )
-        }
+            .forEach {
+                EnergyStorage.SIDED.registerForBlockEntity(
+                    { blockEntity, _ -> blockEntity.energyStorage },
+                    it.get(),
+                )
+            }
     }
 }
