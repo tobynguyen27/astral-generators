d9855e2813ff87f690e88158bdf6ffb723382f3a
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-09 23:52:45 +0700

    register block entity renderer

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/client/AGBlockEntityRenderers.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/client/AGBlockEntityRenderers.kt
@@ -2,6 +2,7 @@ package dev.tobynguyen27.astralgenerators.client
 
 import dev.tobynguyen27.astralgenerators.contents.AGBlockEntities
 import dev.tobynguyen27.astralgenerators.contents.machines.am_controller.AMControllerBlockEntityRenderer
+import dev.tobynguyen27.astralgenerators.contents.machines.multiblock_projector.MultiblockProjectorBlockEntityRenderer
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry
 
 object AGBlockEntityRenderers {
@@ -10,6 +11,10 @@ object AGBlockEntityRenderers {
         BlockEntityRendererRegistry.register(
             AGBlockEntities.AM_CONTROLLER.get(),
             ::AMControllerBlockEntityRenderer,
+        )
+        BlockEntityRendererRegistry.register(
+            AGBlockEntities.MULTIBLOCK_PROJECTOR.get(),
+            ::MultiblockProjectorBlockEntityRenderer
         )
     }
 }
--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/machines/multiblock_projector/MultiblockProjectorBlockEntityRenderer.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/machines/multiblock_projector/MultiblockProjectorBlockEntityRenderer.kt
@@ -1,6 +1,8 @@
 package dev.tobynguyen27.astralgenerators.contents.machines.multiblock_projector
 
 import com.mojang.blaze3d.vertex.PoseStack
+import dev.tobynguyen27.codebebelib.render.CCRenderState
+import dev.tobynguyen27.codebebelib.vec.Matrix4
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
@@ -20,5 +22,8 @@ class MultiblockProjectorBlockEntityRenderer(
         packedLight: Int,
         packedOverlay: Int
     ) {
+        val mat = Matrix4(poseStack)
+        val ccrs = CCRenderState.instance()
+        ccrs.reset()
     }
 }
