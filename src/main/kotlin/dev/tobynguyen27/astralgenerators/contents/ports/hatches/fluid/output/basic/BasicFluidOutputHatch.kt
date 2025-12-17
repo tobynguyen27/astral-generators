b979a080620f8149f5c6a90818b6b695cfdba694
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-17 15:27:07 +0700

    logic for output hatch

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/hatches/fluid/output/basic/BasicFluidOutputHatch.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/hatches/fluid/output/basic/BasicFluidOutputHatch.kt
@@ -1,9 +1,13 @@
 package dev.tobynguyen27.astralgenerators.contents.ports.hatches.fluid.output.basic
 
 import dev.tobynguyen27.astralgenerators.contents.ports.hatches.fluid.FluidHatchBlock
+import dev.tobynguyen27.astralgenerators.contents.ports.hatches.fluid.FluidHatchBlockEntityLogical
 import dev.tobynguyen27.astralgenerators.registry.AGBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class BasicFluidOutputHatch(properties: Properties) : FluidHatchBlock(properties) {
@@ -14,5 +18,19 @@ class BasicFluidOutputHatch(properties: Properties) : FluidHatchBlock(properties
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
         return AGBlockEntities.BASIC_FLUID_OUTPUT_HATCH.create(pos, state)
+    }
+
+    override fun <T : BlockEntity> getTicker(
+        level: Level,
+        state: BlockState,
+        blockEntityType: BlockEntityType<T>
+    ): BlockEntityTicker<T>? {
+        if (level.isClientSide) return null
+
+        return createTickerHelper(
+            blockEntityType,
+            AGBlockEntities.BASIC_FLUID_OUTPUT_HATCH.get(),
+            FluidHatchBlockEntityLogical::serverTick,
+        )
     }
 }
--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/hatches/fluid/output/industrial/IndustrialFluidOutputHatch.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/hatches/fluid/output/industrial/IndustrialFluidOutputHatch.kt
@@ -1,9 +1,13 @@
 package dev.tobynguyen27.astralgenerators.contents.ports.hatches.fluid.output.industrial
 
 import dev.tobynguyen27.astralgenerators.contents.ports.hatches.fluid.FluidHatchBlock
+import dev.tobynguyen27.astralgenerators.contents.ports.hatches.fluid.FluidHatchBlockEntityLogical
 import dev.tobynguyen27.astralgenerators.registry.AGBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class IndustrialFluidOutputHatch(properties: Properties) : FluidHatchBlock(properties) {
@@ -14,5 +18,19 @@ class IndustrialFluidOutputHatch(properties: Properties) : FluidHatchBlock(prope
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
         return AGBlockEntities.INDUSTRIAL_FLUID_OUTPUT_HATCH.create(pos, state)
+    }
+
+    override fun <T : BlockEntity> getTicker(
+        level: Level,
+        state: BlockState,
+        blockEntityType: BlockEntityType<T>
+    ): BlockEntityTicker<T>? {
+        if (level.isClientSide) return null
+
+        return createTickerHelper(
+            blockEntityType,
+            AGBlockEntities.INDUSTRIAL_FLUID_OUTPUT_HATCH.get(),
+            FluidHatchBlockEntityLogical::serverTick,
+        )
     }
 }
