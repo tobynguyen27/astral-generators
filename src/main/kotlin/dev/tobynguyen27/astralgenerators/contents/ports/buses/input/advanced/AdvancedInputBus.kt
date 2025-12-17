2aca1a99058ee7b44f493b6f59447f84e522b9a0
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-17 16:49:25 +0700

    implement logic to buses

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/buses/input/advanced/AdvancedInputBus.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/buses/input/advanced/AdvancedInputBus.kt
@@ -1,9 +1,13 @@
 package dev.tobynguyen27.astralgenerators.contents.ports.buses.input.advanced
 
 import dev.tobynguyen27.astralgenerators.contents.ports.buses.BusBlock
+import dev.tobynguyen27.astralgenerators.contents.ports.buses.BusBlockEntityLogical
 import dev.tobynguyen27.astralgenerators.registry.AGBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class AdvancedInputBus(properties: Properties) : BusBlock(properties) {
@@ -14,5 +18,19 @@ class AdvancedInputBus(properties: Properties) : BusBlock(properties) {
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
         return AGBlockEntities.ADVANCED_INPUT_BUS.create(pos, state)
+    }
+
+    override fun <T : BlockEntity> getTicker(
+        level: Level,
+        state: BlockState,
+        blockEntityType: BlockEntityType<T>,
+    ): BlockEntityTicker<T>? {
+        if (level.isClientSide) return null
+
+        return createTickerHelper(
+            blockEntityType,
+            AGBlockEntities.ADVANCED_INPUT_BUS.get(),
+            BusBlockEntityLogical::serverTick,
+        )
     }
 }
--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/buses/output/basic/BasicOutputBus.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/ports/buses/output/basic/BasicOutputBus.kt
@@ -1,9 +1,13 @@
 package dev.tobynguyen27.astralgenerators.contents.ports.buses.output.basic
 
 import dev.tobynguyen27.astralgenerators.contents.ports.buses.BusBlock
+import dev.tobynguyen27.astralgenerators.contents.ports.buses.BusBlockEntityLogical
 import dev.tobynguyen27.astralgenerators.registry.AGBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class BasicOutputBus(properties: Properties) : BusBlock(properties) {
@@ -14,5 +18,19 @@ class BasicOutputBus(properties: Properties) : BusBlock(properties) {
 
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
         return AGBlockEntities.BASIC_OUTPUT_BUS.create(pos, state)
+    }
+
+    override fun <T : BlockEntity?> getTicker(
+        level: Level,
+        state: BlockState,
+        blockEntityType: BlockEntityType<T?>
+    ): BlockEntityTicker<T?>? {
+        if (level.isClientSide) return null
+
+        return createTickerHelper(
+            blockEntityType,
+            AGBlockEntities.BASIC_OUTPUT_BUS.get(),
+            BusBlockEntityLogical::serverTick,
+        )
     }
 }
