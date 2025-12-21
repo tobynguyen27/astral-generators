d7ffff5a8d2c9bf98f9e65f8d85e0756a06cc92e
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-21 16:30:55 +0700

    fix server error

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/gui/MachineGUI.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/gui/MachineGUI.kt
@@ -3,6 +3,8 @@ package dev.tobynguyen27.astralgenerators.gui
 import dev.tobynguyen27.astralgenerators.core.util.Identifier
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription
 import io.github.cottonmc.cotton.gui.client.BackgroundPainter
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.minecraft.world.Container
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.inventory.ContainerData
@@ -15,15 +17,10 @@ open class MachineGUI(
     blockInventory: Container?,
     propertyDelegate: ContainerData?,
 ) : SyncedGuiDescription(menuType, syncId, playerInventory, blockInventory, propertyDelegate) {
-
-    companion object {
-        private val MACHINE_GUI =
-            BackgroundPainter.createNinePatch(Identifier("textures/gui/machine_gui.png"))
-    }
-
+    @Environment(EnvType.CLIENT)
     override fun addPainters() {
         if (rootPanel != null && !fullscreen) {
-            this.rootPanel.setBackgroundPainter(MACHINE_GUI)
+            this.rootPanel.setBackgroundPainter(BackgroundPainter.createNinePatch(Identifier("textures/gui/machine_gui.png")))
         }
     }
 }
