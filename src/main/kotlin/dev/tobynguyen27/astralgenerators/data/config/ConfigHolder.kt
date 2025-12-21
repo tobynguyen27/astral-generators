f136f08c90ed3d422d817c873361986835672a14
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-21 17:13:00 +0700

    format

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/data/config/ConfigHolder.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/data/config/ConfigHolder.kt
@@ -1,12 +1,11 @@
 package dev.tobynguyen27.astralgenerators.data.config
 
 import me.shedaniel.autoconfig.AutoConfig
-import me.shedaniel.autoconfig.ConfigHolder
 
 object ConfigHolder {
 
-    val CONFIG: AGConfig get() = AutoConfig.getConfigHolder<AGConfig>(AGConfig::class.java).get()
+    val CONFIG: AGConfig
+        get() = AutoConfig.getConfigHolder<AGConfig>(AGConfig::class.java).get()
 
     fun register() {}
-
 }
