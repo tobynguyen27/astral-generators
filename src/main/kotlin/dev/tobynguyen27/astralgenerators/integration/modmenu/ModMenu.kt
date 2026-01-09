209f8c0387e456ee5a04a944f5723f4e64da8bb7
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-09 23:27:09 +0700

    move ModMenu plugin to a folder

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/integration/modmenu/ModMenu.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/integration/modmenu/ModMenu.kt
@@ -1,4 +1,4 @@
-package dev.tobynguyen27.astralgenerators.integration
+package dev.tobynguyen27.astralgenerators.integration.modmenu
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
