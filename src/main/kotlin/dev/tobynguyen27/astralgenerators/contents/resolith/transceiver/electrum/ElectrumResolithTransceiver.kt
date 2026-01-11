8ad38128e85b75c2d77ddecf5b151824ca260928
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-11 17:01:22 +0700

    register ticker

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/resolith/transceiver/electrum/ElectrumResolithTransceiver.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/resolith/transceiver/electrum/ElectrumResolithTransceiver.kt
@@ -1,10 +1,14 @@
 package dev.tobynguyen27.astralgenerators.contents.resolith.transceiver.electrum
 
+import dev.tobynguyen27.astralgenerators.contents.resolith.ResolithBlockEntityLogic
 import dev.tobynguyen27.astralgenerators.contents.resolith.providers.ResolithTier
 import dev.tobynguyen27.astralgenerators.contents.resolith.transceiver.ResolithTransceiver
 import dev.tobynguyen27.astralgenerators.registry.AGBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class ElectrumResolithTransceiver(properties: Properties) : ResolithTransceiver(properties) {
@@ -12,4 +16,18 @@ class ElectrumResolithTransceiver(properties: Properties) : ResolithTransceiver(
         AGBlockEntities.ELECTRUM_RESOLITH_TRANSCEIVER.create(pos, state)
 
     override fun getResolithTier(): ResolithTier = ResolithTier.ELECTRUM
+
+    override fun <T : BlockEntity> getTicker(
+        level: Level,
+        state: BlockState,
+        blockEntityType: BlockEntityType<T>,
+    ): BlockEntityTicker<T>? {
+        if (level.isClientSide) return null
+
+        return createTickerHelper(
+            blockEntityType,
+            AGBlockEntities.ELECTRUM_RESOLITH_TRANSCEIVER.get(),
+            ResolithBlockEntityLogic::serverTick,
+        )
+    }
 }
--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/resolith/transceiver/iron/IronResolithTransceiver.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/resolith/transceiver/iron/IronResolithTransceiver.kt
@@ -1,17 +1,33 @@
 package dev.tobynguyen27.astralgenerators.contents.resolith.transceiver.iron
 
+import dev.tobynguyen27.astralgenerators.contents.resolith.ResolithBlockEntityLogic
 import dev.tobynguyen27.astralgenerators.contents.resolith.providers.ResolithTier
 import dev.tobynguyen27.astralgenerators.contents.resolith.transceiver.ResolithTransceiver
 import dev.tobynguyen27.astralgenerators.registry.AGBlockEntities
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
-import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
-class IronResolithTransceiver(properties: BlockBehaviour.Properties) :
-    ResolithTransceiver(properties) {
+class IronResolithTransceiver(properties: Properties) : ResolithTransceiver(properties) {
     override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity =
         AGBlockEntities.IRON_RESOLITH_TRANSCEIVER.create(pos, state)
 
     override fun getResolithTier(): ResolithTier = ResolithTier.IRON
+
+    override fun <T : BlockEntity> getTicker(
+        level: Level,
+        state: BlockState,
+        blockEntityType: BlockEntityType<T>,
+    ): BlockEntityTicker<T>? {
+        if (level.isClientSide) return null
+
+        return createTickerHelper(
+            blockEntityType,
+            AGBlockEntities.IRON_RESOLITH_TRANSCEIVER.get(),
+            ResolithBlockEntityLogic::serverTick,
+        )
+    }
 }
