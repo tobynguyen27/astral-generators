95dd25d36772bc1075ebbd0d4a11a1b6b8c7ad94
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-21 12:26:53 +0700

    update jvm target for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.loom)
     alias(libs.plugins.kotlin)
@@ -138,10 +141,18 @@ tasks.withType<ProcessResources>().configureEach {
 tasks.withType<JavaCompile>().configureEach { options.encoding = "UTF-8" }
 
 java {
-    toolchain { languageVersion = JavaLanguageVersion.of(17) }
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.GRAAL_VM
+    }
+
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 
     withSourcesJar()
 }
+
+kotlin { compilerOptions { jvmTarget = JvmTarget.JVM_17 } }
 
 tasks.named<Jar>("jar") {
     inputs.property("archivesName", project.base.archivesName)
