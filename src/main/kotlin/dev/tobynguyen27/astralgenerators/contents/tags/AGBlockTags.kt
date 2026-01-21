831b5995c8e8e59a9f83f7f212194c2c20a13c1c
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-21 19:50:32 +0700

    optimize tag utils

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/tags/AGBlockTags.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/tags/AGBlockTags.kt
@@ -1,11 +1,13 @@
 package dev.tobynguyen27.astralgenerators.contents.tags
 
+import dev.tobynguyen27.astralgenerators.AstralGenerators
 import dev.tobynguyen27.astralgenerators.core.util.TagUtils
-import net.minecraft.tags.TagKey
 import net.minecraft.world.level.block.Block
 
 object AGBlockTags {
-    val CASING: TagKey<Block> = TagUtils.create("casings")
+    val CASING = create("casings")
+    val COIL = create("coils")
 
-    val COIL = TagUtils.create("coils")
+    private fun create(name: String) =
+        dev.tobynguyen27.sense.util.TagUtils.createTag<Block>(AstralGenerators.MOD_ID, name)
 }
--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/tags/AGFluidTags.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/tags/AGFluidTags.kt
@@ -1,14 +1,9 @@
 package dev.tobynguyen27.astralgenerators.contents.tags
 
-import net.minecraft.resources.ResourceKey
-import net.minecraft.resources.ResourceLocation
+import dev.tobynguyen27.sense.util.TagUtils
 import net.minecraft.tags.TagKey
 import net.minecraft.world.level.material.Fluid
 
 object AGFluidTags {
-    val STEAM: TagKey<Fluid> =
-        TagKey.create(
-            ResourceKey.createRegistryKey(ResourceLocation("c", "steam")),
-            ResourceLocation("c", "steam"),
-        )
+    val STEAM: TagKey<Fluid> = TagUtils.createCommonTag("steam")
 }
