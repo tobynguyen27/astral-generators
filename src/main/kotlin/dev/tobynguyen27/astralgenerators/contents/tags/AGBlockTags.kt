b32fe57ed6c17811c8ced13207533572e77ac5bf
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-21 22:09:42 +0700

    update tag

--- src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/tags/AGBlockTags.kt
+++ src/main/kotlin/dev/tobynguyen27/astralgenerators/contents/tags/AGBlockTags.kt
@@ -1,13 +1,12 @@
 package dev.tobynguyen27.astralgenerators.contents.tags
 
 import dev.tobynguyen27.astralgenerators.AstralGenerators
-import dev.tobynguyen27.astralgenerators.core.util.TagUtils
+import dev.tobynguyen27.sense.util.TagUtils
 import net.minecraft.world.level.block.Block
 
 object AGBlockTags {
     val CASING = create("casings")
     val COIL = create("coils")
 
-    private fun create(name: String) =
-        dev.tobynguyen27.sense.util.TagUtils.createTag<Block>(AstralGenerators.MOD_ID, name)
+    private fun create(name: String) = TagUtils.createTag<Block>(AstralGenerators.MOD_ID, name)
 }
